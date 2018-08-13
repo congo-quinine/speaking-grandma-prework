fdb0c5bcdf91f952fff24de8ef771cf8bf152497
Rob Parker <congo.quinine@gmail.com>
2018-08-13 14:09:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.capitalize!
     return "HUH?! SPEAK UP, SONNY!"
- elsif phrase == phrase.capitalize!
-    return "NO, NOT SINCE 1938!"
  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
 end
