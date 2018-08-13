8451325f1cd0b0ecde3e94c3f67854bb8777c290
Rob Parker <congo.quinine@gmail.com>
2018-08-13 14:09:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,8 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
+  elsif phrase == phrase.capitalize!
+     return "NO, NOT SINCE 1938!"
 end
 end
 speak_to_grandma('HELLO')
